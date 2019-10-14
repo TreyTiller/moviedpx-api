a18fb6869994aae5dd269bf69e9f710a7682dce9
Trey <treytiller@outlook.com>
2019-10-14 18:13:08 -0400

    Authentication created with .env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const MOVIES = require('./moviedata.json');
@@ -5,6 +6,8 @@ const MOVIES = require('./moviedata.json');
 const app = express()
 
 app.use(morgan('dev'))
+
+console.log(process.env.API_TOKEN);
 
 app.use((req, res) => {
     let response = MOVIES;
