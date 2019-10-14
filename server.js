140f7c819efe76f165d80a1c018dd8da5e5c2c3a
Trey <treytiller@outlook.com>
2019-10-14 18:23:31 -0400

    Validation function created for Token testing in postman

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const MOVIES = require('./moviedata.json');
 const app = express()
 
 app.use(morgan('dev'))
+app.use(function validateBearerToken(req, res, next) {
+    console.log('I will validate bearer token');
+    next();
+})
 
 console.log(process.env.API_TOKEN);
 
-app.use((req, res) => {
+app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIES;
     res.send(response);
 })
+    
+    
+
 
 const PORT = 8000
 
