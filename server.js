efa05d8e957c577fffbee81107b18e2b92e46dce
Trey <treytiller@outlook.com>
2019-10-14 19:04:59 -0400

    Search queries created and tested---conclusion functional

--- server.js
+++ server.js
@@ -23,7 +23,26 @@ console.log(process.env.API_TOKEN);
 
 app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIES;
-    res.send(response);
+    
+    if (req.query.genre) {
+        response = response.filter(movie =>
+          movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+      }
+
+    if (req.query.country) {
+        response = response.filter(movie => 
+        movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            Number(movie.avg_vote)>= Number(req.query.avg_vote)
+            )
+    }
+
+    res.json(response);
 })
 
 
